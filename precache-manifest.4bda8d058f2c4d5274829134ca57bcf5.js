a22ce63b6f28d8dc0340e512e9d742547e259a44
Roman <romanmuhammad7@gmail.com>
2020-05-04 02:54:09 +0500

    Updates

---
+++ precache-manifest.4bda8d058f2c4d5274829134ca57bcf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16c33d35b00d7979fcd1d963b99fd0b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "dc9368097baf1ab2b8f0",
+    "url": "/monster-rolodex/static/css/main.16046e84.chunk.css"
+  },
+  {
+    "revision": "6c716bcfe28bd8fcbb74",
+    "url": "/monster-rolodex/static/js/2.18fc6667.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.18fc6667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9368097baf1ab2b8f0",
+    "url": "/monster-rolodex/static/js/main.2e762482.chunk.js"
+  },
+  {
+    "revision": "cc2f6b8d8ad93a67af8e",
+    "url": "/monster-rolodex/static/js/runtime-main.ece02e8f.js"
+  }
+]);
